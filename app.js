01e6d9940399df2bc05310f4a088026dcfdaaa00
dumto imoh <imohdumto@gmail.com>
2019-05-27 16:54:34 +0100

    added a back to top button

--- app.js
+++ app.js
@@ -100,6 +100,23 @@ function request(data) {
     }
 }
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 /*
